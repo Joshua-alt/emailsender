08e7e1b4deb65ca079cc3fc5a97dbbe62f119382
Joshua-alt <mbavazijoshua@gmail.com>
2022-10-29 19:25:01 +0200

    is mai

--- src/api/sendermail.js
+++ src/api/sendermail.js
@@ -1,38 +1,48 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
 const router = express.Router();
-let transport = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
+let transport = nodemailer.createTransport(smtpTransport({
+	service: 'gmail',
+	host: 'smtp.gmail.com',
+	auth: {
 		user: "mbavazijoshua@gmail.com",
-		pass: "joshua@1998.com"
-    }
-});
+		pass: "nmbmwcfppdowtlks"
+	}
+  }));
 router.get('/', (req, res) => {
   res.json(['ths', 'joshua']);
 });
 
 //send mail
 router.post('/', (req, res) => {
-		console.log('sending email..');
-		res.json(['ths', 'joshua']);
-		const message = {
-	    from: `${req.body.sender}`, // Sender address
-	    to: `${req.body.reciever}`,         // recipients
-	    subject: `${req.body.title}`, // Subject line
-	    text:  `${req.body.message} ` ,// Plain text body
-		html:  `${req.body.messageHtml} ` // html body
-	};
-	console.log('service')
-	console.log('joshua email seder', message)
-	transport.sendMail(message, function(err, info) {
-	    if (err) {
-	      console.log(err)
-	    } else {
-	      console.log('mail has sent.');
-	      console.log(info);
-	    }
-	});
+	    try {
+          
+			
+			const message = {
+				from: `${req.body.sender}`, // Sender address
+				to: `${req.body.reciever}`,         // recipients
+				subject: `${req.body.title}`, // Subject line
+				text:  `${req.body.message} ` ,// Plain text body
+				html:  `${req.body.messageHtml} ` // html body
+			};
+			
+			transport.sendMail(message, function(err, info) {
+				if (err) {
+				console.log(err)
+				res.status(500).json({ message: "Something went wrong" });
+				} else {
+				console.log('mail has sent.');
+				res.status(200).json({ message: "mail has sent." });
+				
+				}
+			});
+		}
+		catch (error) {
+			res.status(500).json({ message: "Something went wrong" });
+			
+			console.log(error);
+		  }
+		
 });
 module.exports = router;
